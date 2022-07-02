d38418e0ed8b051a826d74bcd350568b565077c7
HoangMinhBK <minhmangaka@gmail.com>
2022-07-02 15:32:11 +0700

    new branch back end

--- Lab_04/PageObject.php
+++
@@ -1,6 +0,0 @@
-<?php
-include "PageClass.php";
-$newPage = new Page("New page generated from Class", "2022", "Group 3");
-$newPage->addContent("<h1>This is a page of Group 3</h1>");
-$newPage->createPage();
-?>
