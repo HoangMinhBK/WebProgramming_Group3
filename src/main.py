4ec4c86ad8141a40d853528c0dcc038156bde5ff
dangth <xfa4@github.com>
2022-07-19 18:21:06 +0700

    fix get all comic api

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from sre_constants import SUCCESS
 from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy import Integer
+from sqlalchemy.sql import text
 from sqlalchemy.orm import Session
 from database import SessionLocal, engine
 import models
@@ -8,10 +9,10 @@ import jwt
 from account.forms import AccoutCreateForm
 from typing import Union, Any, List
 from datetime import timedelta, date, datetime
-from models import Account, Author, Comic, Subscribe, Tag, Chapter, Link
+from models import Account, Author, Comic, Subscribe, Tag, Chapter, Link, Tagging
 from pydantic import BaseModel, BaseConfig
 from security import get_account_id, validate_token
-from sqlalchemy import  and_, or_
+from sqlalchemy import and_, or_
 from fastapi.middleware.cors import CORSMiddleware
 
 
@@ -30,7 +31,7 @@ def get_database_session():
 
 
 app = FastAPI()
-BaseConfig.arbitrary_types_allowed = True 
+BaseConfig.arbitrary_types_allowed = True
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -45,7 +46,7 @@ def generate_token(account_id: int) -> str:
         seconds=60 * 60 * 24 * 3  # Expired after 3 days
     )
     to_encode = {
-        "exp": expire, "account_id": account_id, 
+        "exp": expire, "account_id": account_id,
     }
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY,
                              algorithm=SECURITY_ALGORITHM)
@@ -55,11 +56,13 @@ def generate_token(account_id: int) -> str:
 class LoginRequest(BaseModel):
     username: str
     password: str
-'''
+
+
 class LoginReturn(BaseModel):
     token: str
     display_name: str
-    subcribe_list: List[Comic] = []'''
+    subscribe_list: List[Comic] = []
+
 
 @app.post('/login')
 def login(request_data: LoginRequest, db: Session = Depends(get_database_session)):
@@ -69,12 +72,14 @@ def login(request_data: LoginRequest, db: Session = Depends(get_database_session
         raise HTTPException(status_code=404, detail="Acount not found")
     if (record.password == request_data.password):
         token = generate_token(record.account_id)
-        records = db.query(Subscribe).filter(record.account_id == Subscribe.account_id)
+        records = db.query(Subscribe).filter(
+            record.account_id == Subscribe.account_id)
         subcribe_list = list()
         for subcribe in records:
-            comic = db.query(Comic).filter(Comic.comic_id == subcribe.comic_id).first()
+            comic = db.query(Comic).filter(
+                Comic.comic_id == subcribe.comic_id).first()
             subcribe_list.append(comic)
-        
+
         return LoginReturn(token=token, display_name=record.display_name, subcribe_list=subcribe_list)
     else:
         raise HTTPException(status_code=404, detail="User not found")
@@ -88,15 +93,20 @@ async def read_authors(db: Session = Depends(get_database_session)):
 
 @app.get("/comic")
 async def read_comic(db: Session = Depends(get_database_session)):
-    records = db.query(Comic.first_uploaded, Comic.name, Comic.author_id,
-                       Author.name.label("author_name"),
-                       Tag.name.label("tag_name"),
-                       Comic.current_chapter, Comic.rating, Comic.comic_id, Comic.status,
-                       Comic.last_uploaded, Comic.total_view,
-                       Comic.des, Comic.thumbnail).limit(10).all()
+    records = db.execute(
+        "SELECT first_uploaded, c.name as name, c.author_id as author_id, a.name as author_name, t.name as tag_name, current_chapter, rating, c.comic_id as comic_id, status, last_uploaded, total_view, des, thumbnail "
+        + "FROM comic as c INNER JOIN authors as a ON c.author_id = a.author_id "
+        + "INNER JOIN tagging as ta ON c.comic_id = ta.comic_id "
+        + "INNER JOIN tags as t on t.tag_id = ta.tag_id "
+        + "GROUP BY c.comic_id LIMIT 7")
+    
     if records is None:
         raise HTTPException(status_code=404, detail="No comic to display")
-    return records
+    list = []
+    for row in records:
+        list.append(row)
+    return list
+
 
 @app.get("/comic/{comic_id}")
 async def read_single_comic(comic_id: int, db: Session = Depends(get_database_session)):
@@ -105,42 +115,49 @@ async def read_single_comic(comic_id: int, db: Session = Depends(get_database_se
                        Tag.name.label("tag_name"),
                        Comic.current_chapter, Comic.rating, Comic.comic_id, Comic.status,
                        Comic.last_uploaded, Comic.total_view, Comic.thumbnail,
-                       Comic.des).filter(Comic.comic_id == comic_id).limit(1).all()
+                       Comic.des).join(Tag).filter(Comic.comic_id == comic_id).limit(1).all()
     if records is None:
         raise HTTPException(status_code=404, detail="comic not found")
     return records
 
+
 @app.get("/comics/{comic_id}/chapters/{chap_num}")
 async def read_link(comic_id: int, chap_num: int, db: Session = Depends(get_database_session)):
-    records = db.query(Chapter.chapter_id).filter(Chapter.comic_id == comic_id, Chapter.chap_num == chap_num)
+    records = db.query(Chapter.chapter_id).filter(
+        Chapter.comic_id == comic_id, Chapter.chap_num == chap_num)
     records = db.query(Link.link).filter(Link.linkid.in_(records)).all()
     if records is None:
         raise HTTPException(status_code=404, detail="link not found")
     return records
 
+
 @app.post("/subcribe/add/{comic_id}", dependencies=[Depends(validate_token)])
-async def add_subcribe(comic_id: int, account_id : int = Depends(get_account_id),db: Session = Depends(get_database_session)):
+async def add_subcribe(comic_id: int, account_id: int = Depends(get_account_id), db: Session = Depends(get_database_session)):
     record = db.query(Comic).filter(Comic.comic_id == comic_id).first()
     if record is None:
         raise HTTPException(status_code=404, detail="Comic not found")
-    record = db.query(Subscribe).filter(Subscribe.comic_id == comic_id, Subscribe.account_id == account_id).first()
+    record = db.query(Subscribe).filter(Subscribe.comic_id ==
+                                        comic_id, Subscribe.account_id == account_id).first()
     if not record is None:
-        raise HTTPException(status_code=400,detail="User have already subscribed")
+        raise HTTPException(
+            status_code=400, detail="User have already subscribed")
     row = db.query(Subscribe).count()
     subscribe = Subscribe(
-        subscribe_id = row + 1,
-        account_id = account_id,
-        comic_id = comic_id
+        subscribe_id=row + 1,
+        account_id=account_id,
+        comic_id=comic_id
     )
     db.add(subscribe)
     db.commit()
     db.refresh(subscribe)
     return subscribe
 
+
 @app.get("/tags")
 async def read_tags(db: Session = Depends(get_database_session)):
     records = db.query(Tag.name).all()
     return records
+
 
 @app.get("/authors/{author_id}")
 def read_single_author(author_id: int, db: Session = Depends(get_database_session)):
@@ -149,30 +166,34 @@ def read_single_author(author_id: int, db: Session = Depends(get_database_sessio
         raise HTTPException(status_code=404, detail="User not found")
     return record
 
+
 @app.post("/register")
-def register(form : AccoutCreateForm, db: Session = Depends(get_database_session)):
+def register(form: AccoutCreateForm, db: Session = Depends(get_database_session)):
     if not form.display_name:
         raise HTTPException(status_code=400, detail="Name is required")
     if not form.username or not len(form.username) > 3:
-        raise HTTPException(status_code=400, detail="Username should be > 3 characters") 
+        raise HTTPException(
+            status_code=400, detail="Username should be > 3 characters")
     if not form.email or not (form.email.__contains__("@")):
         raise HTTPException(status_code=400, detail="Email is required")
     if not form.password or not len(form.password) >= 6:
-        raise HTTPException(status_code=400, detail="Password must be > 6 characters")
+        raise HTTPException(
+            status_code=400, detail="Password must be > 6 characters")
     row = db.query(Account).count()
     account = Account(
-        account_id = row + 1, 
-        display_name = form.display_name, 
-        username = form.username, 
-        password = form.password, 
-        email = form.email, 
-        created_time = date.today(), 
-        account_type = "normal"
+        account_id=row + 1,
+        display_name=form.display_name,
+        username=form.username,
+        password=form.password,
+        email=form.email,
+        created_time=date.today(),
+        account_type="normal"
     )
-    record = db.query(Account).filter(Account.username == account.username).first()
+    record = db.query(Account).filter(
+        Account.username == account.username).first()
     if record is not None:
         raise HTTPException(status_code=400, detail="Duplicate username")
-    record = db.query(Account).filter(Account.email== account.email).first()
+    record = db.query(Account).filter(Account.email == account.email).first()
     if record is not None:
         raise HTTPException(status_code=400, detail="Duplicate email")
     db.add(account)
