6acd209a1ea68e7165d3f2870f4b5240bfafbc5e
HoangMinhBK <minhmangaka@gmail.com>
2022-07-21 09:54:04 +0700

    final

--- src/layouts/components/Footer/index.jsx
+++ src/layouts/components/Footer/index.jsx
@@ -2,6 +2,7 @@ import { Box, Link, Typography, useMediaQuery } from "@mui/material";
 import { Fragment } from "react";
 import ScrollToTop from "./ScrollToTop";
 import Background from "src/assets/images/footer.jpg";
+import MoibleBackground from "src/assets/images/mobile-footer.jpg";
 
 export default function Footer() {
   const isMobile = useMediaQuery("(max-width: 600px)");
@@ -10,24 +11,35 @@ export default function Footer() {
       <Box
         component="footer"
         sx={{
-          pt: 4,
+          pt: 7,
           display: "flex",
           justifyContent: "flex-start",
           alignItems: "flex-end",
           flexDirection: { xs: "column", sm: "row" },
-          backgroundImage: !isMobile && `url(${Background})`,
-          backgroundPosition: "right",
+          backgroundImage: !isMobile
+            ? `url(${Background})`
+            : `url(${MoibleBackground})`,
+          backgroundPosition: !isMobile ? "right" : "bottom",
           backgroundRepeat: "no-repeat",
-          height: !isMobile && "350px",
-          mb: -13,
+          backgroundSize: isMobile ? "cover" : undefined,
+          height: !isMobile ? "350px" : "200px",
+          mb: !isMobile ? -13 : undefined,
         }}
       >
-        <Box>
+        <Box
+          sx={{
+            position: "relative",
+            background: "white",
+            p: 0.1,
+            borderRadius: "10px",
+            mt: isMobile ? 26 : undefined,
+          }}
+        >
           <Typography
             variant="body2"
             fontFamily="ubuntu"
             align="center"
-            sx={{ mb: 4 }}
+            sx={{ mb: !isMobile ? 4 : 0 }}
           >
             COPYRIGHT © 2022 Hoang Minh - Hai Dang - The Kiet . All rights
             reserved
